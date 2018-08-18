acf493192f114ba31925a1a1fcbced4dbaf36503
Matteo <matteoblombergghini@gmail.com>
2018-08-18 13:27:19 +0200

    Bio is now text instead of string

--- database/migrations/2018_06_18_184546_create_artists_table.php
+++ database/migrations/2018_06_18_184546_create_artists_table.php
@@ -17,7 +17,7 @@ class CreateArtistsTable extends Migration
             $table->increments('id');
             $table->string("stage_name");
             $table->string("real_name")->nullable();
-            $table->string("bio")->nullable();
+            $table->text("bio")->nullable();
             $table->timestamps();
         });
     }
